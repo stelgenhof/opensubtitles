84b626f33c479d178eb5fca0e3ff3d83bcadf4b2
Sacha Telgenhof <me@sachatelgenhof.com>
2020-07-27 18:59:39 +0900

    Updated dependencies. Bumped required PHP version (as 7.1 and 7.2 are out of support). Replaced getenv function call with direct access to env variables (faster) Code cleanup.

--- opensubtitles.php
+++ opensubtitles.php
@@ -1,13 +1,17 @@
 #!/usr/bin/env php
 <?php
+declare(strict_types=1);
+
 require 'vendor/autoload.php';
 
 use Dotenv\Dotenv;
+use Dotenv\Exception\InvalidPathException;
 use fXmlRpc\Client as fXmlRpcClient;
 use fXmlRpc\Parser\NativeParser;
 use fXmlRpc\Serializer\NativeSerializer;
 use fXmlRpc\Transport\HttpAdapterTransport;
 use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\GuzzleException;
 use Http\Adapter\Guzzle6\Client as AdapterGuzzle6Client;
 use Http\Message\MessageFactory\GuzzleMessageFactory;
 use Illuminate\Cache\CacheManager;
@@ -15,11 +19,9 @@ use Illuminate\Container\Container;
 use Illuminate\Filesystem\Filesystem;
 use League\CLImate\CLImate;
 
-const APP_NAME    = 'OpenSubtitles Downloader';
-const APP_VERSION = '0.2';
+const APP_NAME = 'OpenSubtitles Downloader';
+const APP_VERSION = '0.3';
 $app_name = APP_NAME . ' v' . APP_VERSION;
-
-\date_default_timezone_set('Asia/Tokyo');
 
 // Start CLI
 $cli = new CLImate;
@@ -28,39 +30,45 @@ $cli->clear();
 $cli->description($app_name);
 $cli->lightGreen($app_name);
 $cli->lightGreen()->border('-*-', \strlen($app_name));
-
-$input  = $cli->input('Please enter the IMDB Movie Number:');
-$imdbID = $input->prompt();
+$cli->clearLine();
 
 // Load the configuration
-$env = Dotenv::create(__DIR__);
-$env->load();
+try {
+    $env = Dotenv::create(__DIR__);
+    $env->load();
+} catch (InvalidPathException $e) {
+    $cli->error(\sprintf('ERROR: %s', $e->getMessage()));
+    exit();
+}
 
 // Initialize the filesystem/cache
-$container           = new Container;
+$container = new Container;
 $container['config'] = [
-    'cache.default'     => 'file',
+    'cache.default' => 'file',
     'cache.stores.file' => [
         'driver' => 'file',
-        'path'   => __DIR__ . '/_cache'
+        'path' => __DIR__ . '/_cache'
     ]
 ];
 
 $container['files'] = new Filesystem;
-$cacheManager       = new CacheManager($container);
-$cache              = $cacheManager->store();
+$cacheManager = new CacheManager($container);
+$cache = $cacheManager->store();
 
 // Initialize the HTTP and XMLRPC Clients
 $httpClient = new GuzzleClient();
-$client     = new fXmlRpcClient(
-    \getenv('OPENSUBTITLES_API_URL'),
+$client = new fXmlRpcClient(
+    $_ENV['OPENSUBTITLES_API_URL'],
     new HttpAdapterTransport(new GuzzleMessageFactory(), new AdapterGuzzle6Client($httpClient)),
     new NativeParser(),
     new NativeSerializer()
 );
 
+$input = $cli->input('Please enter the IMDB Movie Number:');
+$imdbID = $input->prompt();
+
 try {
-    $cache_key = \md5(\getenv('OPENSUBTITLES_LANGUAGES') . $imdbID);
+    $cache_key = \md5($_ENV['OPENSUBTITLES_LANGUAGES'] . $imdbID);
 
     $response = $cache->get($cache_key);
 
@@ -69,17 +77,17 @@ try {
 
         // Login to OpenSubtitles
         $response = $client->call('LogIn', [
-            \getenv('OPENSUBTITLES_USERNAME'),
-            \getenv('OPENSUBTITLES_PASSWORD'),
+            $_ENV['OPENSUBTITLES_USERNAME'],
+            $_ENV['OPENSUBTITLES_PASSWORD'],
             'en',
-            \getenv('OPENSUBTITLES_USER_AGENT')
+            $_ENV['OPENSUBTITLES_USER_AGENT']
         ]);
 
         // Proceed if ok and token is provided
-        if ('200 OK' === $response['status'] && ! empty($response['token'])) {
+        if ('200 OK' === $response['status'] && !empty($response['token'])) {
             $response = $client->call(
                 'SearchSubtitles',
-                [$response['token'], [['sublanguageid' => \getenv('OPENSUBTITLES_LANGUAGES'), 'imdbid' => $imdbID]]]
+                [$response['token'], [['sublanguageid' => $_ENV['OPENSUBTITLES_LANGUAGES'], 'imdbid' => $imdbID]]]
             );
 
             $cache->put($cache_key, $response, 60);
@@ -103,13 +111,13 @@ try {
             ));
 
             $movieDir = $hit['MovieName'] . ' - ' . $hit['MovieYear'];
-            if (! $container['files']->isDirectory($movieDir)) {
+            if (!$container['files']->isDirectory($movieDir)) {
                 $container['files']->makeDirectory($movieDir);
             }
 
             // Download subtitle file if not yet downloaded
             $subtitleFile = $movieDir . '/' . \basename($hit['SubDownloadLink']);
-            if (! \is_readable($subtitleFile)) {
+            if (!\is_readable($subtitleFile)) {
                 try {
                     $httpClient->request(
                         'GET',
@@ -119,7 +127,7 @@ try {
                 } catch (Exception $e) {
                     $cli->error($e->getMessage());
                     exit();
-                } catch (\GuzzleHttp\Exception\GuzzleException $e) {
+                } catch (GuzzleException $e) {
                     $cli->error($e->getMessage());
                     exit();
                 }
@@ -130,8 +138,8 @@ try {
 
             $srtContents = $container['files']->get($srtFile);
 
-            if (\getenv('OPENSUBTITLES_TARGET_ENCODING') !== $hit['SubEncoding']) {
-                $srtContents = \iconv($hit['SubEncoding'], \getenv('OPENSUBTITLES_TARGET_ENCODING'), $srtContents);
+            if ($_ENV['OPENSUBTITLES_TARGET_ENCODING'] !== $hit['SubEncoding']) {
+                $srtContents = \iconv($hit['SubEncoding'], $_ENV['OPENSUBTITLES_TARGET_ENCODING'], $srtContents);
             }
             $container['files']->put(
                 $movieDir . '/' . $hit['IDSubtitleFile'] . '-' . $hit['MovieName'] . '.' . $hit['LanguageName'] . '.srt',
@@ -142,9 +150,9 @@ try {
             $container['files']->delete($srtFile);
         }
     } else {
-        $languages = \implode(', ', \array_map(function ($a) {
+        $languages = \implode(', ', \array_map(static function ($a) {
             return \locale_get_display_language($a, 'en');
-        }, \explode(',', \getenv('OPENSUBTITLES_LANGUAGES'))));
+        }, \explode(',', $_ENV['OPENSUBTITLES_LANGUAGES'])));
 
         $cli->error(\sprintf(
             'No %s subtitles found for IMDB ID %s. Please make sure to provide valid IMDB ID.',
@@ -152,14 +160,14 @@ try {
             $imdbID
         ));
     }
-} catch (Exception $e) {
-    $cli->error($e->getMessage());
+} catch (Exception | \Psr\SimpleCache\InvalidArgumentException $e) {
+    $cli->error(\sprintf('ERROR: %s', $e->getMessage()));
 }
 
 $cli->br()->info('Completed.');
 
 /**
- * Uncompresses a GZipped file
+ * Inflates a GZipped file
  *
  * @param string $srcName the filepath of the original GZipped file
  * @param string $dstName the filepath of the uncompressed file (destination)
@@ -167,9 +175,9 @@ $cli->br()->info('Completed.');
 function uncompress($srcName, $dstName)
 {
     $sfp = \gzopen($srcName, 'rb');
-    $fp  = \fopen($dstName, 'wb');
+    $fp = \fopen($dstName, 'wb');
 
-    while (! \gzeof($sfp)) {
+    while (!\gzeof($sfp)) {
         $string = \gzread($sfp, 4096);
         \fwrite($fp, $string, \strlen($string));
     }
